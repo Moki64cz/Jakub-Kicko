60db979db3f9c661dc36fbd9c6eb3470bd79e9e1
Moki <j.kicko64@gmail.com>
2024-09-12 08:18:49 +0200

    First Commit - Testy 1

---
+++ Test.py
@@ -0,0 +1,16 @@
+balls = int(input("Balls count?: "))
+
+nice = 69
+
+if balls == 2:
+    print("good")
+elif balls == 1:
+    print("Consumed")
+elif balls == 69:
+    print(f"The balls are {nice}")
+elif balls > 2:
+    print("Radiation balls!")
+elif balls < 1:
+    print("NO BALLS!")
+else:
+    print("Magic")
